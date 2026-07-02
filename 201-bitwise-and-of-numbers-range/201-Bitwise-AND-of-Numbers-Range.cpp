360c26f78bc6ebed10893d37d8cd7ea087721265
Bhavesh <bhaveshyt.infinity@gmail.com>
2026-07-02 21:34:25 +0530

    [201] [Time Beats: 5.07%] [Memory Beats: 47.70%] - LeetPush

---
+++ 201-bitwise-and-of-numbers-range/201-Bitwise-AND-of-Numbers-Range.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+        int count = 0;
+
+        while(left != right) {
+            left >>= 1;
+            right >>= 1;
+            count++;
+        }
+
+        while(count--) {
+            left <<= 1;
+            right <<= 1;
+        }
+
+        return left;
+    }
+};
