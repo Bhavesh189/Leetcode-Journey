844169a951ce4204701233f5a28ab1872d934113
Bhavesh <bhaveshyt.infinity@gmail.com>
2026-07-02 11:16:38 +0530

    [191] [Time Beats: -%] [Memory Beats: -%] - LeetPush

--- Number-of-1-Bits.cpp
+++ Number-of-1-Bits.cpp
@@ -2,11 +2,12 @@
 2public:
 3    int hammingWeight(int n) {
 4        int ans = 0;
-5        for(int i = 0; i < 32; i++) {
-6            if(n&1) ans++;
-7            n >>= 1;
-8        }
-9
-10        return ans;
-11    }
-12};
+5        
+6        while(n != 0) {
+7            n = n & (n-1);
+8            ans++;
+9        }
+10
+11        return ans;
+12    }
+13};
