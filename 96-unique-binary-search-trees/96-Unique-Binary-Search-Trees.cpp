0b02f74fb38687718ba11cfcb1fa91099f607ca3
Bhavesh <bhaveshyt.infinity@gmail.com>
2026-07-09 12:45:25 +0530

    [96] [Time Beats: 100.00%] [Memory Beats: 89.70%] - LeetPush

---
+++ 96-unique-binary-search-trees/96-Unique-Binary-Search-Trees.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+
+    int dp[20] = {0};
+
+    int numTrees(int n) {
+        if(n <= 1) return 1;
+        if(dp[n] != 0) return dp[n];
+
+        int ans = 0;
+
+        for(int i = 1; i <= n; i++) {
+            ans += numTrees(i-1) * numTrees(n-i);
+        }
+
+        return dp[n] = ans;
+    }
+};
