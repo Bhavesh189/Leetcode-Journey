c10f1032e1e48297b3a55342cf591a3edd1d8562
Bhavesh <bhaveshyt.infinity@gmail.com>
2026-08-02 17:08:41 +0530

    [20] [Time Beats: 100.00%] [Memory Beats: 38.25%] - LeetPush

---
+++ 20-valid-parentheses/20-Valid-Parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(char ch : s) {
+            if(ch == '(' || ch == '[' || ch == '{') {
+                st.push(ch);
+            } else {
+
+                if(st.empty()) return false;
+
+                if(ch == ')') {
+                    if(st.top() == '(') st.pop();
+                    else st.push(ch);
+                } else if(ch == ']') {
+                    if(st.top() == '[') st.pop();
+                    else st.push(ch);
+                } else if(ch == '}') {
+                    if(st.top() == '{') st.pop();
+                    else st.push(ch);
+                }
+            }
+        }
+
+        return st.empty();
+    }
+};
