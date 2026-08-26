f5b4c6637600b73f9fe7ff3aa5e55fa367129a34
Bhavesh <bhaveshyt.infinity@gmail.com>
2026-08-26 22:58:43 +0530

    [1] [Time Beats: 72.52%] [Memory Beats: 42.97%] - LeetPush

---
+++ 1-two-sum/1-Two-Sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+
+        for(int i = 0; i < nums.size(); i++) {
+            int a = nums[i];
+            int b = -(a-target);
+
+            if(mp.find(b) != mp.end()) {
+                return {i, mp[b]};
+            }
+
+            mp[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
