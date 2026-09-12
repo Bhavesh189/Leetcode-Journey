ac1841a1fc12df818e933f6963ef21d773477330
Bhavesh <bhaveshyt.infinity@gmail.com>
2026-09-12 20:32:01 +0530

    [455] [Time Beats: 59.12%] [Memory Beats: 47.94%] - LeetPush

---
+++ 455-assign-cookies/455-Assign-Cookies.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int ans = 0;
+        int m = g.size(), n = s.size();
+        
+        int i = 0, j = 0;
+
+        while(i < m && j < n) {
+            if(s[j] >= g[i]) {
+                ans++;
+                i++;
+                j++;
+            }
+            else j++;
+        }
+
+        return ans;
+    }
+};
