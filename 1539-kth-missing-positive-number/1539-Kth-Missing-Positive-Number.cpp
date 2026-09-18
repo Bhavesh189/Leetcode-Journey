20dac25b14567139ff126a50e6fe9f2b48cb0ecf
Bhavesh <bhaveshyt.infinity@gmail.com>
2026-09-18 23:33:12 +0530

    [1539] [Time Beats: 100.00%] [Memory Beats: 61.05%] - LeetPush

---
+++ 1539-kth-missing-positive-number/1539-Kth-Missing-Positive-Number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int n = arr.size();
+        int s = 0, e = n-1;
+
+        while(s <= e) {
+            int mid = s + (e-s)/2;
+
+            int missing = arr[mid]-(mid+1);
+
+            if(missing >= k) {
+                e = mid-1;
+            } else {
+                s = mid+1;
+            }
+        }
+
+        return s+k;
+    }
+};
